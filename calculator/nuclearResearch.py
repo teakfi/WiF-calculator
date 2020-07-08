56cb643036087070541359db9d42129a983be347
Tuomo Kalliokoski <tuokall@gmail.com>
2020-07-09 00:45:10 +0300

    some restructuring in atomic research rolling and new function to calculate how many throws one needs to buy in a turn to get research done within turn limit

--- calculator/nuclearResearch.py
+++ calculator/nuclearResearch.py
@@ -4,13 +4,44 @@ Nuclear research for World in Flames
 
 Created on Fri Jul  3 16:50:25 2020
 
+number of turns = number of times the nuclear research rolls are done
+
 @author: tuokall
 """
 
 import die
+import math
 
-   
-def howManyRollsInTurnToGet(target, turns, certainty=0.95, start=0):
+def rollRun(target,numberOfTrials,turns, rolls,start):
+    successCount=0
+    i=0
+    while(i < numberOfTrials):
+        sum=start
+        rollcount=0
+        for turn in range(turns):
+            sum+=die.bestXfromRolls(1, rolls, 10)[0]
+            
+        if(sum>=target):
+            successCount+=1
+        i+=1    
+        
+    return successCount
+
+
+def rollRunToGetCert(target,turns,certainty,start):
+    rolls=1
+    while(True):
+        numberOfTrials=5000
+        successCount=rollRun(target,numberOfTrials,turns,rolls,start)
+            
+        if(successCount/numberOfTrials>certainty):
+            break
+        else:
+            rolls+=1
+
+    return rolls,successCount/numberOfTrials
+    
+def simpleHowToGet(target, turns, certainty=0.95, start=0):
     try:
         if((target-start)/turns>10):
             raise ValueError
@@ -18,33 +49,45 @@ def howManyRollsInTurnToGet(target, turns, certainty=0.95, start=0):
         print('Not possible to achieve result {0} with starting value {1} in {2} turns'.format(target,start,turns))
         return 0
     else:
-        rolls=1
-        while(True):
-            numberOfTrials=10000
-            successCount=0
-            i=0
-            while(i < numberOfTrials):
-                sum=start
-                turn=1
-                for turn in range(turns):
-                    sum+=die.bestXfromRolls(1, rolls, 10)[0]
-                
-                if(sum>=target):
-                    successCount+=1
-                i+=1
-            if(successCount/numberOfTrials>certainty):
-                break
-            else:
-                rolls+=1
+        return rollRunToGetCert(target,turns,certainty,start)
+    
 
-        return rolls,successCount/numberOfTrials
+def howToGet(target, turns, certainty=0.95, start=0):
+    high, origcert=simpleHowToGet(target, turns, certainty, start)
+    low = high - 1
+    changeturn=turns//2
+    change=changeturn
+    while(True):
+        numberOfTrials=5000
+        i=0
+        successCount=0
+        while(i < numberOfTrials):
+            sum=start
+            for turn in range(changeturn):
+                sum+=die.bestXfromRolls(1, high, 10)[0]
+            for turn in range(changeturn, turns):
+                sum+=die.bestXfromRolls(1, low, 10)[0]
+            if(sum>=target):
+                successCount+=1
+            i+=1
+        runcertainty=successCount/numberOfTrials
+        if(math.isclose(runcertainty,certainty,abs_tol=0.001)):
+            break
+        if(change==1):
+            break
+        change=change//2
+        if(runcertainty>certainty):
+            changeturn-=change
+        else:
+            changeturn+=change
+    return high, low, changeturn
     
 
 
-'''
-testing
+#testing
 
-print(howManyRollsInTurnToGet(101, 10))
-print(howManyRollsInTurnToGet(100,15))
-print(howManyRollsInTurnToGet(100, 15,0.5))
-'''
+print(simpleHowToGet(101, 10))
+print(simpleHowToGet(100,15))
+print(simpleHowToGet(100, 15,0.5))
+print(howToGet(100,15))
+
